e892f5e8db6f023588e121bab0fb2ce7bde6b10e
linelijah3 <elijahlin3@gmail.com>
2022-12-04 23:24:21 -0500

    Create medicalassistance.cpp
    first preliminary draft of idea from initial report

---
+++ medicalassistance.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+int main() {
+    cout << "Welcome to Medical Assistance's Care Rec Tool"
+            "---------------------------------------------"
+            "1) Make a Recommendation"
+            "2) Print Database"
+            "3) Exit"<<endl;
+    cout << "---------------------------------------------"
+            "Please make a selection:" << endl;
+    //cin >> thing;
+    cout << "---------------------------------------------";
+    cout << "Which of these criteria is important to you?"
+            "1) Location"
+            "2) Cost of Care"
+            "3) Patient Ratings"
+            "4) Timeliness"
+            "5) Publicly or Privately Owned"
+            "6) Safety"
+            "---------------------------------------------"<<endl;
+    cout << "Please make a selection:" << endl;
+    //cin >> other thing;
+}
