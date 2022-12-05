ef0d307e17f1e6f0ee8ec4a27a0b0327aecde98f
linelijah3 <elijahlin3@gmail.com>
2022-12-05 14:19:17 -0500

    removed redundancy
    removed redundancy for old names

--- hospitalList.cpp
+++ hospitalList.cpp
@@ -2,34 +2,34 @@
 
 void hospitalList::setHospitalDataString(string& name, string &stringData, const string& category) {
     for (auto &i :mainList) {
-        if (i.first==name) {
-            i.second.setString(stringData, category);
+        if (i.returnString("name")==name) {
+            i.setString(stringData, category);
         }
     }
 }
 
 string hospitalList::initializeHospital(string &name, string& oldName) {
     for (auto &i :mainList) {
-        if (i.first==name) {
+        if (i.returnString("name")==name) {
             for (int j = 2; j < 1000; j++) {
                 string newName = name + " (" + to_string(j) + ")";
                 bool isFound = false;
                 for (auto &k: mainList) {
-                    if (k.first == newName) {
+                    if (k.returnString("name") == newName) {
                         isFound = true;
                     }
                 }
                 if (!isFound) {
-                    mainList.push_back(make_pair(oldName, hospital(newName, oldName)));
+                    mainList.push_back(hospital(newName, oldName));
                     return newName;
                 }
             }
         }
     }
-    mainList.push_back(make_pair(name,hospital(name,oldName)));
+    mainList.push_back(hospital(name,oldName));
     return name;
 }
 
-vector<pair<string, hospital>> hospitalList::accessList() {
+vector<hospital> hospitalList::accessList() {
     return this->mainList;
 }
--- hospitalList.h
+++ hospitalList.h
@@ -4,9 +4,9 @@
 #include "hospital.h"
 class hospitalList {
 private:
-    vector<pair<string, hospital>> mainList;
+    vector<hospital> mainList;
 public:
     void setHospitalDataString(string& name, string& stringData, const string& category);
     string initializeHospital(string& name, string& oldName);
-    vector<pair<string, hospital>> accessList();
+    vector<hospital> accessList();
 };
